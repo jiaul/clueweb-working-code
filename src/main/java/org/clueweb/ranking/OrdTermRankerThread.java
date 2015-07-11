141d2089a590b3092a6ae1b25e8833a901091dc4
jiaul <jiaul@jiaul.(none)>
2015-07-11 15:24:32 -0400

    further tweaks

--- src/main/java/org/clueweb/ranking/OrdTermRankerThread.java
+++ src/main/java/org/clueweb/ranking/OrdTermRankerThread.java
@@ -20,6 +20,8 @@ public class OrdTermRankerThread implements Runnable {
   private String [] keys;
   private Score [] allScore;
   private int [] doclen;
+  private int[] qtid = new int[100];
+  private double[] idf =  new double[100];
   
   public OrdTermRankerThread(String name, int start, int end, String [] keys, int [][] docs, int [][] freq, int [] doclen, Query query, int numTopDoc, int thid, Score [] allScore){
       threadName = name;
@@ -34,10 +36,13 @@ public class OrdTermRankerThread implements Runnable {
       this.thid = thid;
       this.allScore = allScore;
       qlen = query.TermID.size();
+      for(int i = 0; i < qlen; i++) {
+    	  idf[i] = query.idf.get(i);
+    	  qtid[i] = query.TermID.get(i);
+      }
   }
   
   public void run() {
-    float score;
     PriorityQueue<Score> scoreQueue = new PriorityQueue<Score>(numTopDoc, new Comparator<Score>() {
       public int compare(Score a, Score b) {
          if(a.score < b.score)
@@ -53,15 +58,106 @@ public class OrdTermRankerThread implements Runnable {
     float k1 = 1.0f;
     float b = 0.5f;
     float adl = 450.0f;
+    float score = 0.0f;
      for(int i = start; i < end; i++) {
-      score = 0.0f;
-      for(int j = 0; j < qlen; j++) {
-       	 int indx = Arrays.binarySearch(docs[i], query.TermID.get(j));
-       	 if(indx >= 0) {
+      score = 0.0f; 
+      if(qlen == 1) {
+    	  int indx = Arrays.binarySearch(docs[i], qtid[0]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[0] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+        	 }
+      }
+      else if(qlen == 2) {
+    	  int indx = Arrays.binarySearch(docs[i], qtid[0]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[0] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+        	 }
+    	  indx = Arrays.binarySearch(docs[i], qtid[1]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[1] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+        	 }
+      }
+      else if(qlen == 3) {
+    	  int indx = Arrays.binarySearch(docs[i], qtid[0]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[0] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+        	 }
+    	  indx = Arrays.binarySearch(docs[i], qtid[1]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[1] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+    	  indx = Arrays.binarySearch(docs[i], qtid[2]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[2] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+      }
+      else if(qlen == 4) {
+    	  int indx = Arrays.binarySearch(docs[i], qtid[0]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[0] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+        	 }
+    	  indx = Arrays.binarySearch(docs[i], qtid[1]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[1] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+    	  indx = Arrays.binarySearch(docs[i], qtid[2]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[2] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+    	  indx = Arrays.binarySearch(docs[i], qtid[3]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[3] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+      }
+      else if(qlen == 5) {
+    	  int indx = Arrays.binarySearch(docs[i], qtid[0]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[0] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+        	 }
+    	  indx = Arrays.binarySearch(docs[i], qtid[1]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[1] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+    	  indx = Arrays.binarySearch(docs[i], qtid[2]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[2] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+    	  indx = Arrays.binarySearch(docs[i], qtid[3]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[3] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+    	  indx = Arrays.binarySearch(docs[i], qtid[4]);
+    	  if(indx >= 0) {
+              int tf = freq[i][indx];
+              score += idf[4] * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
+          }
+      }
+      else {
+        for(int j = 0; j < qlen; j++) {
+       	   int indx = Arrays.binarySearch(docs[i], query.TermID.get(j));
+       	   if(indx >= 0) {
              int tf = freq[i][indx];
              score += query.idf.get(j) * ((k1+1.0f) * tf)/(k1*(1.0f-b+b*doclen[i]/adl)+tf);
-       	 }
+       	   }
+        }
       }
+      
+      if(score <= 0.0f)
+    	  continue;
       
       if(n < numTopDoc) {
          scoreQueue.add(new Score(keys[i], score, query.qno));
@@ -74,7 +170,7 @@ public class OrdTermRankerThread implements Runnable {
          }
        }
      }
-     // print top 10 results
+     // take top k results
      int scoreQSize = Math.min(n, scoreQueue.size());
      int spos = numTopDoc * thid;
      for(int k = 0; k < scoreQSize; k++) {
